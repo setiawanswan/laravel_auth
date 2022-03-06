d66990bc0fbc5612637b2805c40f7a1c3e0534dd
Setiawan Setiawan <setiawan.tumilin@gmail.com>
2022-03-06 20:23:30 +0700

    post controller & tinker

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,41 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Setiawan",
-            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam rerum dignissimos quas possimus inventore. Dolor voluptate, totam provident debitis placeat et, omnis sunt neque rerum odit labore dolores eius ullam, reprehenderit aliquid hic adipisci dolorum! Ipsam deleniti dolores quidem ducimus temporibus, veniam quis voluptatum nihil enim similique est praesentium facilis non possimus aspernatur architecto nulla culpa quod exercitationem ratione porro. Animi incidunt veritatis impedit possimus deserunt quidem. Molestias debitis officia provident nihil deserunt aliquid cupiditate ratione? Enim alias adipisci suscipit?",
-        ],
-        [
-            "title" => "Judul Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Setiawan Jodi",
-            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam rerum dignissimos quas possimus inventore. Dolor voluptate, totam provident debitis placeat et, omnis sunt neque rerum odit labore dolores eius ullam, reprehenderit aliquid hic adipisci dolorum! Ipsam deleniti dolores quidem ducimus temporibus, veniam quis voluptatum nihil enim similique est praesentium facilis non possimus aspernatur architecto nulla culpa quod exercitationem ratione porro. Animi incidunt veritatis impedit possimus deserunt quidem. Molestias debitis officia provident nihil deserunt aliquid cupiditate ratione? Enim alias adipisci suscipit?",
-        ],
-    ];
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        // $post = [];
-        // foreach($posts as $p) {
-        //     if($p["slug"] === $slug) {
-        //         $post = $p;
-        //     }
-        // }
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    use HasFactory;
 }
